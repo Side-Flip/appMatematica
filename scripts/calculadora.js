bfa5b893ab1dd6e2a1fd2779184443e61714471e
Camilo Molina <milo_0922@outlook.com>
2021-09-13 09:03:48 -0500

    Añadidas las funcionalidades basicas

--- scripts/calculadora.js
+++ scripts/calculadora.js
@@ -1,3 +1,26 @@
-$('#btn-clear').on('click', function (event){
-    alert("comemeloshuevos");
-})
+const input = "#calc_display";
+
+$("button").click(function (e) { 
+    var button_value = $(this).val();
+    switch(button_value){
+        case "enter":
+            var input_value = $(input).val();
+            var output = enter(input_value);
+            $(input).val(output);
+            break;
+        case "clear":
+            limpiar();
+            break;
+        default:
+            $(input).val($(input).val()+button_value);
+    }
+});
+
+function enter(value){
+    var expresion = eval(value);
+    return expresion;
+}
+
+function limpiar(){
+    $(input).val("");
+}
